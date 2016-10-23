a01babf9b9f7193add21cf142dc3216aa1b1a07d
biratkarki1 <coolrabit22@gmail.com>
2016-10-23 11:03:07 +0545

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,55 @@
+/*
+Student Name= Birat Bikram Karki
+Roll no= 9
+Lab no= 01
+Program= 
+Date= 23,oct
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+int a,g,result;
+
+char c;
+
+printf("enter (+,-,*,/)");
+scanf("%c",&c);
+
+printf("enter the first number");
+scanf("%d",&a);
+
+printf("enter second number");
+scanf("%d",&g);
+
+switch(c){
+	case '+':
+			result=a+g;
+			printf("sum of two number is:%d",result);
+			break;
+		
+	case '-':
+			result=a-g;
+			printf("sum of two number is:%d",result);
+			break;
+			
+			
+	case '*':
+			result=a*g;
+			printf("sum of two number is:%d",result);
+			break;		
+			
+				
+	case '/':
+			result=a/g;
+			result=a/g;
+			printf("sum of two number is:%d",result);
+			break;		
+				
+			
+}
+
+	return 0;
+}
